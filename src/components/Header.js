4bbfe6e6a141e056ca525a19c50c2efc34d148dc
arbdt <67365149+arbdt@users.noreply.github.com>
2021-01-06 13:37:53 +1030

    uplaoding <Header> component file

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import "../styles/Header.css";
+
+// this class component file is for the header of the page
+class Header extends Component {
+  // render component content
+  render() {
+    return (
+      <header className="header">
+        <h1>Employee Directory</h1>
+      </header>
+    )
+  }
+}
+
+// export component
+export default Header;
